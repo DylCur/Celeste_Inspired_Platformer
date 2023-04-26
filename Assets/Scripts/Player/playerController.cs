eb1880cc98360df0fac11f039005486fa9172fb2
DylCur <dylancurry001@gmail.com>
2023-04-26 07:14:52 +0100

    Stuff
    Some Stuff

--- Assets/Scripts/Player/playerController.cs
+++ Assets/Scripts/Player/playerController.cs
@@ -5,84 +5,150 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
 
-    [Header("Keycodes")]
-    public KeyCode jumpKey = KeyCode.Space;
-    public KeyCode consoleJumpKey = KeyCode.Joystick1Button2;
+
+
 
     [Header("Movement Parameters")]
-    public float playerSpeed = 12f;
 
-    [Header("Grounded Parameters")]
-    public float radius = 0.2f;
-    public LayerMask groundLayers;
-    public bool isGrounded;
+    public float playerSpeed = 12f;
+    public bool canMove = true;
+
 
     [Header("Jumping Parameters")]
-    public float jumpForce = 10f;
+
+    public bool isGrounded;
+    public float jumpForce = 12f;
+    public LayerMask groundMask;
+    public float circleRadius = 0.1f;
+
+
+    [Header("Keycodes")]
+
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode dashKey = KeyCode.LeftShift;
+    
 
     [Header("Dashing Parameters")]
-    public float dashSpeed = 10f;
-    public float dashDuration = 0.5f;
-    public float dashCooldown = 1f;
 
-    private Vector2 dashDirection;
-    private bool isDashing;
-    private float dashTime;
-    private bool hasDashedGrounded;
+    public bool canDash;
+    public bool isDashing;
+    public float dashForce;
+
+
+    float horizontalInput;
+    float verticalInput;
+
 
     Rigidbody2D rb;
-    public float HorizontalInput;
+
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        isGrounded = Physics2D.OverlapCircle(transform.position, radius, groundLayers);
-
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(transform.position, radius, groundLayers);
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        HorizontalInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(HorizontalInput * playerSpeed, rb.velocity.y);
+        isGrounded = Physics2D.OverlapCircle(transform.position, circleRadius, groundMask);
+        
+        if(Input.GetKeyDown(dashKey) && !isDashing){
+            dash();
+        }
+        movement(canMove); // Left Right and Jumping
+    }
 
-        if (Input.GetKeyDown(jumpKey) && isGrounded || Input.GetKeyDown(consoleJumpKey) && isGrounded)
-        {
+    public void movement(bool canMove){
+
+        if(canMove){
+            rb.velocity = new Vector2(horizontalInput * playerSpeed, rb.velocity.y);
+        }
+
+        else{
+            rb.velocity = Vector2.zero;
+        }
+
+        if(Input.GetKeyDown(jumpKey) && isGrounded){
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+        
+    }
 
-        if ((Input.GetKeyDown(jumpKey) || Input.GetKeyDown(consoleJumpKey)) && !isDashing && !hasDashedGrounded && !isGrounded)
-        {
-            float horizontalInput = Input.GetAxisRaw("Horizontal");
-            float verticalInput = Input.GetAxisRaw("Vertical");
+    public void dash(){
 
-            if (horizontalInput != 0 || verticalInput != 0)
-            {
-                dashDirection = new Vector2(horizontalInput, verticalInput).normalized;
-                isDashing = true;
-                dashTime = dashDuration;
-            }
-        }
+        isDashing = true;
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (isDashing)
-        {
-            rb.velocity = dashDirection * dashSpeed;
-            dashTime -= Time.deltaTime;
+        // freezePlayer(0, Vector2.zero);
+        Debug.Log("Hello");
 
-            if (dashTime <= 0f)
-            {
-                isDashing = false;
-                hasDashedGrounded = !isGrounded;
-            }
-        }
+        // if(horizontalInput > 0){ // If right
+        //     Debug.Log($"Horizontal: {horizontalInput}");
+        //     if(verticalInput > 0){ // If up-right
+        //         rb.velocity = new Vector2(dashForce, dashForce);
+        //     }
 
-        if (isGrounded)
-        {
-            hasDashedGrounded = false;
-        }
+        //     else if(verticalInput < 0){
+        //         rb.velocity = new Vector2(dashForce, -dashForce);
+        //     }
+
+        //     else{
+        //         rb.velocity = new Vector2(dashForce, 0f);
+        //     }
+
+            
+        // }
+        
+        // else if(horizontalInput < 0){
+        //     if(verticalInput > 0){ // If up-right
+        //         rb.velocity = new Vector2(-dashForce, dashForce);
+        //     }
+
+        //     else if(verticalInput < 0){
+        //         rb.velocity = new Vector2(-dashForce, -dashForce);
+        //     }
+
+        //     else{
+        //         rb.velocity = new Vector2(-dashForce, 0f);
+        //     }
+        // }
+
+        // else{
+        //     if(verticalInput > 0){ // If up-right
+        //         rb.velocity = new Vector2(0, dashForce);
+        //     }
+
+        //     else if(verticalInput < 0){
+        //         rb.velocity = new Vector2(0, -dashForce);
+        //     }
+
+        //     else{
+        //         rb.velocity = new Vector2(dashForce, 0f);
+        //     }
+        // }
+
+    Vector2 dashDirection = new Vector2(horizontalInput, verticalInput).normalized;
+
+    // Set the player's velocity to the dash direction multiplied by the dash force
+    rb.velocity = dashDirection * dashForce;
+
+    isDashing = false;
+
+
+    }
+
+    void freezePlayer(float gravity, Vector2 newVelocity){
+
+        
+
+        
     }
 }
